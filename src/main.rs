b8fae4cf88c9d0664926a5f3f5309dd665c6c769
Brendan Dagys <brendandagys@gmail.com>
2023-07-09 15:09:04 -0400

    Prevent empty submissions and understand need for cloning better

--- src/main.rs
+++ src/main.rs
@@ -25,40 +25,45 @@ fn app() -> Html {
         })
     };
 
-    let send_api_request = Callback::from(move |_| {
+    let send_api_request = {
         let message = (*message_handle).clone();
-        let response_handle = response_handle.clone();
 
-        wasm_bindgen_futures::spawn_local(async move {
-            let response: Vec<i32> = Request::post(API_ENDPOINT)
-                .body(message.replace("[", "").replace("]", "").replace(" ", ""))
-                .unwrap()
-                .send()
-                .await
-                .unwrap()
-                .json()
-                .await
-                .unwrap();
+        Callback::from(move |_| {
+            let message = message.clone();
+            let response_handle = response_handle.clone();
 
-            response_handle.set(format!("{:?}", response));
-        });
-    });
+            wasm_bindgen_futures::spawn_local(async move {
+                let response: Vec<i32> = Request::post(API_ENDPOINT)
+                    .body(message.replace("[", "").replace("]", "").replace(" ", ""))
+                    .unwrap()
+                    .send()
+                    .await
+                    .unwrap()
+                    .json()
+                    .await
+                    .unwrap();
+
+                response_handle.set(format!("{:?}", response));
+            });
+        })
+    };
 
     let onkeydown = {
+        let message = (*message_handle).clone();
         let send_api_request = send_api_request.clone();
 
         Callback::from(move |e: KeyboardEvent| {
-            if e.key() == "Enter" {
+            if e.key() == "Enter" && !message.trim().is_empty() {
                 send_api_request.emit(());
             }
         })
     };
 
-    let onclick = {
-        Callback::from(move |_| {
+    let onclick = Callback::from(move |_| {
+        if !(*message_handle).trim().is_empty() {
             send_api_request.emit(());
-        })
-    };
+        }
+    });
 
     html!(
         <div class="box">
@@ -67,15 +72,13 @@ fn app() -> Html {
                 <img src="static/favicon.png" alt="logo" />
                 <div>
                     <input ref={input_node_ref} {oninput} {onkeydown} />
-                    <button {onclick}>{"Send"}</button>
+                    <button {onclick}>{"Sort"}</button>
                 </div>
 
                 <small>{"Enter comma-separated numbers to be sorted..."}</small>
             </div>
 
-            <div class="response">
-                <h1>{response}</h1>
-            </div>
+            <div class="response"><h1>{response}</h1></div>
         </div>
     )
 }
